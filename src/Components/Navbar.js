3681cddbb796f5d4c99022f1397112ec86778b6d
Matt <mmaguire333@gmail.com>
2023-06-07 17:07:04 -0600

    Create a Navbar component with a title and a home button

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h1>Where's Waldo</h1>
+            <button type="button">Home</button>
+        </div>
+    )
+}
+
+export default Navbar;
