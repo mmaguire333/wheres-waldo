f529c1eba381970d247695dc0fefbbb5e1f8245c
Matt <mmaguire333@gmail.com>
2023-06-07 17:39:41 -0600

    Add a BrowerRouter component to App which contains a Navbar component, a route to the Home component, and three routes for three level components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Level from './Components/Level';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/level-one" element={<Level level={1}/>} />
+          <Route path="/level-two" element={<Level level={2}/>} />
+          <Route path="/level-three" element={<Level level={3}/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
