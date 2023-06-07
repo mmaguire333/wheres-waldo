d707b249c931a418857ba304a480ea325acf8d5f
Matt <mmaguire333@gmail.com>
2023-06-07 17:14:52 -0600

    Create a LevelCard compnent which the user will click on to access the levels of the game

---
+++ src/Components/LevelCard.js
@@ -0,0 +1,16 @@
+
+
+const LevelCard = () => {
+    return (
+        <div className="LevelCard">
+            <image src="" alt="" />
+            <h2>Level: _</h2>
+            <p>Difficulty: _______</p>
+            <div className="character-list">
+                <p>list of characters</p>
+            </div>
+        </div>
+    );
+}
+
+export default LevelCard;
