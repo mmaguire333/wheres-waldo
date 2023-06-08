647d74fbb1948a0d5560cfc8471ba8f268a27adf
Matt <mmaguire333@gmail.com>
2023-06-07 20:24:46 -0600

    Display level title, preview image, and characters in each LevelCard

--- src/Components/LevelCard.js
+++ src/Components/LevelCard.js
@@ -1,13 +1,86 @@
+import levelOneImage from '../Assets/Level-Images/level-one-preview.png';
+import levelTwoImage from '../Assets/Level-Images/level-two-preview.jpeg';
+import levelThreeImage from '../Assets/Level-Images/level-three-preview.jpg';
+import waldo from '../Assets/Characters/Waldo.jpg';
+import odlaw from '../Assets/Characters/Odlaw.jpg';
+import wizard from '../Assets/Characters/Wizard.jpg';
+import gandalf from '../Assets/Characters/Gandalf.jpg'
+import aragorn from '../Assets/Characters/Aragorn.jpg'
+import boromir from '../Assets/Characters/Boromir.jpg'
+import legolas from '../Assets/Characters/Legolas.jpg'
+import gimli from '../Assets/Characters/Gimli.jpg'
+import frodo from '../Assets/Characters/Frodo.jpg'
+import sam from '../Assets/Characters/Sam.jpg'
+import merry from '../Assets/Characters/Merry.jpg'
+import pippin from '../Assets/Characters/Pippin.jpg'
+import '../Styles/LevelCard.css';
+
+const LevelCard = (props) => {
+    let imageSrc;
+    let difficulty;
+    let title;
+    let characters = [];
+    switch(props.level) {
+        case 1:
+            imageSrc = levelOneImage;
+            difficulty = 'Easy';
+            title = 'Classic Waldo';
+            characters = [{name: 'Waldo', charSrc: waldo}];
+            break;
+        case 2: 
+            imageSrc = levelTwoImage;
+            difficulty = 'Medium';
+            title = 'Waldo at the Beach';
+            characters = [
+                {name: 'Waldo', charSrc: waldo},
+                {name: 'Odlaw', charSrc: odlaw},
+                {name: 'Wizard Whitebeard', charSrc: wizard}
+            ];
+            break;
+        case 3:
+            imageSrc = levelThreeImage;
+            difficulty = 'Hard';
+            title = 'Lord of the Rings Edition';
+            characters = [
+                {name: 'Waldo', charSrc: waldo},
+                {name: 'Gandalf', charSrc: gandalf},
+                {name: 'Aragorn', charSrc: aragorn},
+                {name: 'Boromir', charSrc: boromir},
+                {name: 'Legolas', charSrc: legolas},
+                {name: 'Gimli', charSrc: gimli},
+                {name: 'Frodo', charSrc: frodo},
+                {name: 'Sam', charSrc: sam},
+                {name: 'Merry', charSrc: merry},
+                {name: 'Pippin', charSrc: pippin}
+            ]
+            break;
+        default: 
+            imageSrc = '';
+            difficulty = '';
+            title = '';
+            characters = [];
+            break;
+    }
 
 
-const LevelCard = () => {
     return (
         <div className="LevelCard">
-            <image src="" alt="" />
-            <h2>Level: _</h2>
-            <p>Difficulty: _______</p>
-            <div className="character-list">
-                <p>list of characters</p>
+            <h2>{title}</h2>
+            <img className="card-image" src={imageSrc} alt="Level Preview" />
+            <div className="level-info">
+                <h3>Level {props.level}</h3>
+                <h3>{difficulty}</h3>
+            </div>
+            <h3 className="character-title">Characters:</h3>
+            <div className="characters">
+                {characters.map((char) => {
+                    return (
+                        <div className="character-container" key={`level-${props.level}-${char.name}`}>
+                            <img src={char.charSrc} alt={char.name} />
+                        </div>
+                        
+                    );
+                })}
             </div>
         </div>
     );
