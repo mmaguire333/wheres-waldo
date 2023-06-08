34d22a24d4aa3c7e0eb5b449358ad45c8e13fc9b
Matt <mmaguire333@gmail.com>
2023-06-08 15:23:31 -0600

    Create a Popup list coponent that will contain a circle around where the user clicks and list of characters that have not yet been found

---
+++ src/Components/PopupList.js
@@ -0,0 +1,12 @@
+
+
+const PopupList = (props) => {
+    return (
+        <div className="PopupList">
+            <div className="circle"></div>
+            <ul className="list"></ul>
+        </div>
+    )
+}
+
+export default PopupList;
