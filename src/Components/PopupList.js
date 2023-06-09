aad322f859a814b534b69fc9953d5b6ef424e497
Matt <mmaguire333@gmail.com>
2023-06-09 16:32:36 -0600

    Add top and left positions to the circle div from coordinates prop, add a list of characters below the circle, and conditionally render the PopupList only when the active prop is true

--- src/Components/PopupList.js
+++ src/Components/PopupList.js
@@ -1,12 +1,25 @@
-
+import '../Styles/PopupList.css';
 
 const PopupList = (props) => {
-    return (
-        <div className="PopupList">
-            <div className="circle"></div>
-            <ul className="list"></ul>
-        </div>
-    )
+    if(props.active) {
+        return (
+            <div className="PopupList" style={{left: props.coordinates.x, top: props.coordinates.y}}>
+                <div className="circle"></div>
+                <ul className="list">
+                    {props.characters.map((char) => {
+                        return (
+                            <li key={char.name}>
+                                <p>{char.name}</p>
+                                <img src={char.charSrc} alt={char.name} />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    } else {
+        return null;
+    }
 }
 
 export default PopupList;
