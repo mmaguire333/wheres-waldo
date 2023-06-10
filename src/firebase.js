6f5e82a3a2dd965dd7230085fde4e3f55ff43f02
Matt <mmaguire333@gmail.com>
2023-06-09 19:29:58 -0600

    Rename firebase-config.js to firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-8t4hhVFgPX-GDwG5bT-386vvK1Lkqyo",
+    authDomain: "where-s-waldo-8e944.firebaseapp.com",
+    projectId: "where-s-waldo-8e944",
+    storageBucket: "where-s-waldo-8e944.appspot.com",
+    messagingSenderId: "1030932198638",
+    appId: "1:1030932198638:web:5fb1c0909cda2ccd546fff"
+}
+
+export function getFirebaseConfig() {
+    if (!firebaseConfig || !firebaseConfig.apiKey) {
+        // eslint-disable-next-line no-useless-concat
+        throw new Error('No Firebase configuration object provided.' + '\n' +
+        'Add your web app\'s configuration object to firebase-config.js');
+      } else {
+        return firebaseConfig;
+      }
+}
