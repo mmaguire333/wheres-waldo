0aab77d554b915f225cbc3a6497beb5fc60fdbfb
Matt <mmaguire333@gmail.com>
2023-06-14 16:38:15 -0600

    Move Sam's marker to prevent overlap with Frodo's marker

--- src/Components/CharacterMarker.js
+++ src/Components/CharacterMarker.js
@@ -6,7 +6,9 @@ const CharacterMarker = (props) => {
             {props.characters.map((char) => {
                 let charCoordinates = props.markerCoordinates.find(coord => coord.name === char.name);
                 if(char.discovered === true) {
-                    return <p key={char.name} style={{left: charCoordinates.x, top: charCoordinates.y}}>{char.name}</p>
+                    // Ternary prevents Sam and Frodos markers from overlapping without changing Sam's actual coordinates
+                    return (char.name === 'Sam' ? <p key={char.name} style={{left: charCoordinates.x - 20, top: charCoordinates.y}}>{char.name}</p>
+                                                : <p key={char.name} style={{left: charCoordinates.x, top: charCoordinates.y}}>{char.name}</p>);
                 } else {
                     return null;
                 }
