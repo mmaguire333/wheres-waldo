397b4b07f732739d8039f517ddc44fa67589d5df
Matt <mmaguire333@gmail.com>
2023-06-16 17:54:42 -0600

    Implement handleSubmit function

--- src/Components/WinForm.js
+++ src/Components/WinForm.js
@@ -1,21 +1,26 @@
+import { useNavigate } from 'react-router-dom';
 import '../Styles/WinForm.css'
-
+import { uploadUserScore } from '../firebase';
 
 const WinForm = (props) => {
-    const handleSubmit = (e) => {
+    const navigate = useNavigate();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        uploadUserScore(document.getElementById('name-input').value, props.score, props.level);
+        navigate("/");
     }
 
     if(props.active) {
         return (
             <div className="WinForm">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="win-message">
                         <h2>You Won!</h2>
                         <p>Enter your name to save your score.</p>
                     </div>
                     <input type="text" id="name-input" placeholder="Your Name" required></input>
-                    <button type="button" id="submit-button" onClick={handleSubmit}>Submit</button>
+                    <button type="submit" id="submit-button">Submit</button>
                 </form>
             </div>
         )
