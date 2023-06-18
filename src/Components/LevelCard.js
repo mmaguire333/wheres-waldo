57de1b949600644e9b24a1fbbd1e4bd53a18af06
Matt <mmaguire333@gmail.com>
2023-06-18 16:49:57 -0600

    Change color of difficulty text based on the value of difficulty

--- src/Components/LevelCard.js
+++ src/Components/LevelCard.js
@@ -18,18 +18,21 @@ import '../Styles/LevelCard.css';
 const LevelCard = (props) => {
     let imageSrc;
     let difficulty;
+    let difficultyColor;
     let title;
     let characters = [];
     switch(props.level) {
         case 1:
             imageSrc = levelOneImage;
             difficulty = 'Easy';
+            difficultyColor = 'green';
             title = 'Classic Waldo';
             characters = [{name: 'Waldo', charSrc: waldo}];
             break;
         case 2: 
             imageSrc = levelTwoImage;
             difficulty = 'Hard';
+            difficultyColor = 'orange';
             title = 'Waldo at the Beach';
             characters = [
                 {name: 'Waldo', charSrc: waldo},
@@ -39,7 +42,8 @@ const LevelCard = (props) => {
             break;
         case 3:
             imageSrc = levelThreeImage;
-            difficulty = 'Almost Impossible';
+            difficulty = 'Very Hard';
+            difficultyColor = 'red';
             title = 'Lord of the Rings Edition';
             characters = [
                 {name: 'Gandalf', charSrc: gandalf},
@@ -56,6 +60,7 @@ const LevelCard = (props) => {
         default: 
             imageSrc = '';
             difficulty = '';
+            difficultyColor = 'black';
             title = '';
             characters = [];
             break;
@@ -68,7 +73,7 @@ const LevelCard = (props) => {
             <img className="card-image" src={imageSrc} alt="Level Preview" />
             <div className="level-info">
                 <h3>Level {props.level}</h3>
-                <h3>{difficulty}</h3>
+                <h3 style={{color: `${difficultyColor}`}}>{difficulty}</h3>
             </div>
             <h3 className="character-title">Characters:</h3>
             <div className="characters">
