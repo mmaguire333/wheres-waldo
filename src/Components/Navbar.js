e0f6b41d993028b33cab5754cce837d191ee2bb8
Matt <mmaguire333@gmail.com>
2023-06-18 20:05:18 -0600

    Add the new waldo image to the header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import waldoHead from '../Assets/Characters/waldo-head.png';
 import '../Styles/Navbar.css';
 
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <h1>Where's Waldo</h1>
+            <div className="container-left">
+                <h1>Where's Waldo</h1>
+                <img id="waldo-nav-image" src={waldoHead} alt=""/>
+            </div>
             <Link to="/"><button className="home-button" type="button">Home</button></Link>
         </div>
     )
